37e0bcc40e74d9e671ac9d62c55135c7fb6dc351
LeftTale <73308966+LeftTale@users.noreply.github.com>
2020-11-12 00:51:27 +0000

    Version 1
    Added player character along with movement script character controller and a start on faux gravity

---
+++ Planet Game/Assets/Scripts/PlanetGravity.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGravity : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D rbPlayer;
+    public bool inRange = false;
+    public GameObject planetBody;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       rbPlayer = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (inRange)
+        {
+            rbPlayer.gravityScale = 0f;
+            rbPlayer.AddForce(new Vector2((planetBody.transform.position.x - player.transform.position.x), (planetBody.transform.position.y - player.transform.position.y)));
+        }
+        else if (!inRange)
+            rbPlayer.gravityScale = 3f;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("attracted");
+            inRange = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inRange = false;
+        }
+    }
+}
---
+++ Planet Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public GameObject planet;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("Jump", true);
+        }
+
+        Debug.DrawLine(transform.position, planet.transform.position);
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } 
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime,crouch,jump);
+        jump = false;
+    }
+
+    public void OnLanding ()
+    {
+        animator.SetBool("Jump", false);
+    }
+}
